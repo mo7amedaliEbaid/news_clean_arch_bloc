1dfb110b64a9344d779a90ccbda0e0a04fcd8d57
Mohamed Ali <mo7amedaliebaid@gmail.com>
2023-02-16 08:21:26 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/news_clean_arch_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.news_clean_arch_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/resources/data_state.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+abstract class DataState<T> {
+  final T? data;
+  final DioError? error;
+
+  const DataState({this.data, this.error});
+}
+
+class DataSuccess<T> extends DataState<T> {
+  const DataSuccess(T data) : super(data: data);
+}
+
+class DataError<T> extends DataState<T> {
+  const DataError(DioError error) : super(error: error);
+}
